ff31a4afbf3c09f4dbc4c330f6489c2d060b800d
Anthony Moore <antmoe@Anthonys-MacBook-Pro.local>
2018-02-20 15:00:55 -0500

    home page

---
+++ javascript/camera/cam.js
@@ -0,0 +1,13 @@
+jQuery(function(){
+  
+  jQuery('#camera_wrap_4').camera({
+    height: 'auto',
+    loader: 'none',
+    pagination: false,
+    thumbnails: false,
+    hover: false,
+    time: 4000,
+    opacityOnGrid: false,
+    imagePath: 'images/'
+  });
+});
